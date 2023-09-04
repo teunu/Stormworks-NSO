19ee10dc7740cb2295bf5962d3104919adc392dd
Player <reyalp.evisneffo@gmail.com>
2023-09-04 23:37:21 +0200

    Some ploppables placed, ploppable blender file changes etc

--- rom/creatorToolkitData/CurveSpec/Lib/Colours.lua
+++ rom/creatorToolkitData/CurveSpec/Lib/Colours.lua
@@ -9,13 +9,14 @@ local C = Color255
 --- @class Colours
 local Colours = {
 
-	Gravel	 = C(63, 61, 53),
-	Concrete = C(199, 185, 170),
-	Grey     = C(80, 80, 80),
-	Rock     = C(77, 77, 62),
-	Grass    = C(54, 64, 7),
-	Wood     = C(42, 11, 3),
-	Rusty    = C(98, 70, 60),
+	Gravel	   = C(63, 61, 53),
+	Concrete   = C(199, 185, 170),
+	Grey       = C(80, 80, 80),
+	Rock       = C(77, 77, 62),
+	Grass      = C(54, 64, 7),
+	Wood       = C(42, 11, 3),
+	Rusty      = C(98, 70, 60),
+	Industrial = C(102, 94, 92),
 
 }
 
